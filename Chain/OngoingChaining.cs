36886dedd61b33e68eb77a1065636de14ebcbbcb
Marcos Vinicius Maia <juninhowblu@gmail.com>
2017-12-29 17:08:35 -0200

    Added error fallback methods

--- Chain/OngoingChaining.cs
+++ Chain/OngoingChaining.cs
@@ -4,16 +4,44 @@ namespace Chain
 {
     public class OngoingChaining<T> where T : class
     {
-        private  T _currentResult;
+        private T _currentResult;
+        private Action<Exception> _errorFallback;
+        private Action<Exception> _globalErrorFallback;
 
         internal OngoingChaining(T currentResult)
         {
             _currentResult = currentResult;
         }
 
+        public OngoingChaining<T> WithErrorFallback(Action<Exception> onError)
+        {
+            _errorFallback = onError;
+            return this;
+        }
+
+        public OngoingChaining<T> WithGlobalErrorFallback(Action<Exception> onError)
+        {
+            _globalErrorFallback = onError;
+            return this;
+        }
+
         public OngoingChaining<TNext> Then<TNext>(Func<T, TNext> nextFunction) where TNext : class
         {
-            return _currentResult != null ? new OngoingChaining<TNext>(nextFunction(_currentResult)) : new OngoingChaining<TNext>(null);
+            try
+            {
+                var result = _currentResult != null
+                    ? new OngoingChaining<TNext>(nextFunction(_currentResult))
+                    : new OngoingChaining<TNext>(null);
+
+                _errorFallback = null;
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _errorFallback?.Invoke(ex);
+                _globalErrorFallback?.Invoke(ex);
+                return null;
+            }
         }
 
         public OngoingChaining<T> OrElse(T fallback)
